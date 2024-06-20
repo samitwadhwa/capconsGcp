bef70b477152b8fbd2dddb804d09dac1b8b69fa2
CodingWOnderer <vipin@capcons.com>
2024-06-20 12:34:58 +0530

    Build Bugs Fixed

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -22,7 +22,7 @@ function Main() {
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
+              {` Make changes to your profile here. Click save when you're done.`}
             </DialogDescription>
           </DialogHeader>
           <div className="grid gap-4 py-4">
