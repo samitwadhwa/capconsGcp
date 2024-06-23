8bb5dc0e6e31db1a6e93cfb306e02cedf3570162
CodingWOnderer <vipin@capcons.com>
2024-06-23 10:19:13 +0530

    otp Error FIxed

--- app/(auth)/forgetpassword/page.tsx
+++ app/(auth)/forgetpassword/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -13,13 +13,12 @@ import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 function ForgetPassword() {
-
   const form = useForm({
     defaultValues: {
       otp: "",
     },
   });
- 
+
   const onSubmit = (data: any) => {
     console.log(data);
     toast.success(`Success , Your Otp code is : ${data.otp}`);
@@ -32,7 +31,10 @@ function ForgetPassword() {
           <div className="grid gap-2 text-left">
             <h1 className="text-2xl font-bold mt-8">Forget Password</h1>
             <div className="flex space-x-2">
-              <p className='text-p font-p text-[#D6A7FF]'> Trouble with logging in?</p>
+              <p className="text-p font-p text-[#D6A7FF]">
+                {" "}
+                Trouble with logging in?
+              </p>
             </div>
           </div>
           <div className="grid gap-4">
@@ -44,8 +46,11 @@ function ForgetPassword() {
                 required
               />
             </div>
-          
-            <Button type="submit" className="w-full mt-4 border border-[#D6A7FF]  !rounded bg-[#2F2D2D hover:bg-[#D6A7FF]">
+
+            <Button
+              type="submit"
+              className="w-full mt-4 border border-[#D6A7FF]  !rounded bg-[#2F2D2D hover:bg-[#D6A7FF]"
+            >
               Send OTP
             </Button>
           </div>
@@ -53,49 +58,56 @@ function ForgetPassword() {
             <p className="text-balance text-[#A0A0A0]">
               Enter your 6 digit code sent to ********82
             </p>
-           
           </div>
 
           <Form {...form}>
-          <form className="grid gap-2" onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="otp"
-              render={({ field }) => (
-                <FormControl>
-                  <>
-                    <FormItem>
-                      <OtpStyledInput
-                        numInputs={6}
-                        inputType="number"
-                        {...field}
-                        value={field.value}
-                        onChange={field.onChange}
-                      />
-                    </FormItem>
-                    <FormMessage />
-                  </>
-                </FormControl>
-              )}
-            />
-              <div className='flex justify-start items-center mt-3'>
-          <h6 className='h6 text-sm mr-2 text-white'>Didn&#39;t get a code?</h6>
-          <h6 className='text-[#D6A7FF] text-sm h6 cursor-pointer underline'><a href="/signup">Resend</a></h6>
-        </div>
-            <Button type="submit" className="mt-3 font-bold">Verify</Button>
-          </form>
-        </Form>
+            <form className="grid gap-2" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="otp"
+                render={({ field }) => (
+                  <FormControl>
+                    <>
+                      <FormItem>
+                        <OtpStyledInput
+                          {...field}
+                          inputType={"number"}
+                          numInputs={6}
+                          className="p-0"
+                        />
+                      </FormItem>
+                      <FormMessage />
+                    </>
+                  </FormControl>
+                )}
+              />
+              <div className="flex justify-start items-center mt-3">
+                <h6 className="h6 text-sm mr-2 text-white">
+                  Didn&#39;t get a code?
+                </h6>
+                <h6 className="text-[#D6A7FF] text-sm h6 cursor-pointer underline">
+                  <a href="/signup">Resend</a>
+                </h6>
+              </div>
+              <Button type="submit" className="mt-3 font-bold">
+                Verify
+              </Button>
+            </form>
+          </Form>
 
-          <div className='flex items-center mt-5'>
-      <div className='flex-grow border-t border-[#A0A0A0]'></div>
-      <span className='mx-2 text-[#A0A0A0] h6 text-sm'>or continue with</span>
-      <div className='flex-grow border-t  border-[#A0A0A0]'></div>
-      </div>
+          <div className="flex items-center mt-5">
+            <div className="flex-grow border-t border-[#A0A0A0]"></div>
+            <span className="mx-2 text-[#A0A0A0] h6 text-sm">
+              or continue with
+            </span>
+            <div className="flex-grow border-t  border-[#A0A0A0]"></div>
+          </div>
 
-      <div className="flex space-x-2">
-              <p className='text-sm font-p text-[#D6A7FF] underline'><a href="/signup">Sign in with different account?</a></p>
-        </div>
-        
+          <div className="flex space-x-2">
+            <p className="text-sm font-p text-[#D6A7FF] underline">
+              <a href="/signup">Sign in with different account?</a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
