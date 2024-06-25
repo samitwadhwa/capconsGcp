f8d202c87e7f4a0649442046150fa165c0c8a58d
Samit Wadhwa <samitwadhwa@Samits-ARM-MacBook-Pro.local>
2024-06-26 01:56:24 +0530

    features images added

--- lib/data.ts
+++ lib/data.ts
@@ -77,7 +77,7 @@ export const ReviewCardsData = [
 export const FeaturedCardData: FeaturedCardProps[] = [
     {
       imageUrl:
-        "https://s3-alpha-sig.figma.com/img/0e1b/351d/280ac08b8831570af15bb256d997a184?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CE~N6mr5U3rlw9MoJH96xcfB5~FWS68hvojw0kfxgZISEIybPJIBn6v0ZxHj8y6H-ZXG~Qhfp89bv-Kw52A2jWKB4ICv6fkbtmEMjhE7LOVblhGxn12uIAgP2EUi0N3i1ozan9k2gM5PQUZrngaXcsyGlEoZ9Rg5jd7SQLfb56zAjLC05xnnylhqjZ~l1wo6rIwiSNncekslPyKV5nXdcpOOsbJNRARoIef85UMAwzNgnNGwAXsEl0~x1nwBZgl413jSwKMlzRs0B4zcEcTC7UrY0rEcw06vkxpDk43xPhSdFE~boTMxv-5c6QnzQrMp5wSUfkYKWOFqgIMoHNlZRg__",
+        "https://storage.googleapis.com/capcons-analytics/Capcons-dev-images/FeaturesCard1.webp",
       title: "Connect!",
       reverse: false,
       index: "01",
@@ -86,7 +86,7 @@ export const FeaturedCardData: FeaturedCardProps[] = [
     },
     {
       imageUrl:
-        "https://s3-alpha-sig.figma.com/img/fec9/f7e8/0aa15d544f25bc0ea54461639ddbfebe?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bB~tO4RLCOvLiJGi3NUmdXoXWYZXK~5Gg2YalRq08JQFea-cznbLR1ZSuebdcpvx2789rsv-pqqLxPCWN7-3iqbaudg3wC6fKKxqAJgCLXTs3OJVj3rdS-SoRDHj79G1tgeeT9fLTUXf34KwHsLqiqMzDlpUTynCApj~4KrrGnIwho7J6d-E4dwyJqjZAI0gQmnudTorakSmsFdijhiuN6AxZesLbOGkabGoPk~be5Jl56cvXxC7UQ4KsXns6mhQ8v6a7Omu0DDAPns0Y4YpYahBOhDowCVjTDH5SBMHbYoQs4Cf1Ean74u9P-DQWawi3R-phofHWRYw2yfaRMRUsg__",
+        "https://storage.googleapis.com/capcons-analytics/Capcons-dev-images/FeaturesCard2.webp",
       title: "Wedrop!",
       reverse: true,
       index: "02",
@@ -95,7 +95,7 @@ export const FeaturedCardData: FeaturedCardProps[] = [
     },
     {
       imageUrl:
-        "https://s3-alpha-sig.figma.com/img/a136/9069/83d632a2b38e22a96d1653a2817fd977?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qTf74LA7emMXtyL4C4xcnkXf6Cc792aeyQZ6yfEdlTFnyBy0LX3794OV7ogYtNTlUX4S~Ry7pd-0UKTqXy8szH0MGYxzci7clLi9V6tXbNUqzcb8fyjgPbxnQ9MRlWgAUQu-N8OPqZWtNaanuyJ37L-Fzev3vP3enxsC90KfEG1fwB4vrB8ZgF5tv0jdFndjAEn1mX3utn39HB~m-JApAVawWzrUQkFq2TsqS-n5~x9VXREgpXWsIFKuyrmyjPNgjgn560Hx9qgmWT1X5JwTOUm4EggStiKOX7K33wzl~bRBOTAIexZqmMVvbUgY8WJJb~7e38v1jGogjFp~RoTRcw__",
+        "https://storage.googleapis.com/capcons-analytics/Capcons-dev-images/FeaturesCard3.webp",
       title: "Icebreakers!",
       reverse: false,
       index: "03",
@@ -104,7 +104,7 @@ export const FeaturedCardData: FeaturedCardProps[] = [
     },
     {
       imageUrl:
-        "https://s3-alpha-sig.figma.com/img/0216/deab/0b5456d66e3b331a77951ac40f640c0e?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aI4OWUY4ACezWXxppm8m0mzjA1~F~hZF16ju0F2NOhsZlZqZpTMTfJ~DmlJ8C0FAEILgLn8xw1eAdF1BOAab2O0vlsICdJicYKRqq0PFzUBIedAO8aU~M4GpGcYFku1DG5-fLDLPaaUHK0yP4~v9~lVsqxj08TsBMSFi2iTnRUVCs4dRZTkW0Ie3v2~Ahucn-lpukAfwhQN44sAQ6mUa38RxV0mc0gd9uwNhapA6DZ9c8XLUsd4Qw~Lht72b3goUFUAWpTXSR-bxLhsKzbuAwgBiSHPcN-OxpevOHBzJnadJoeHalbHXVAPWZr3ozso7DQOEBfeyhQuTDnc1zIdrEw__",
+        "https://storage.googleapis.com/capcons-analytics/Capcons-dev-images/FeaturesCard4.webp",
       title: "Community",
       reverse: true,
       index: "04",
@@ -113,7 +113,7 @@ export const FeaturedCardData: FeaturedCardProps[] = [
     },
     {
       imageUrl:
-        "https://s3-alpha-sig.figma.com/img/6252/6821/e147cd97187dc89448dadb951a9edf16?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=L1FXropFlgioQ0JgrhChTIWAM2JOyQxI53jn3kEUNb9v8hJlYZtkATMwLESLH6kGTzQEdEbWS0IGjDa5TI5COxIa1lqksjjcJr1TGtveArSRgYdXrKK9W2~w2cYWqGXcvubFoO-Fuv56GRQZ89kycawUF4Tke2xIwyedkaUdROl1Iqorlt-NfbzE8TZsPppOrD5qyfIcL2ThzI9uyc3~aSBrxjcoHTH6PCtuBQycAHuWcxtfpkVnISTP0SZ-Rk7iztHOYgGu57FoU8qwTdgK4XqjDQYk~RbKXYHU~cS6C0Y9ys~Meqat3kAu7FP4I9w7gdS6qTj5CukXCgeHbQC9kA__",
+        "https://storage.googleapis.com/capcons-analytics/Capcons-dev-images/FeaturesCard5.webp",
       title: "Match your interest!",
       reverse: false,
       index: "05",
@@ -122,7 +122,7 @@ export const FeaturedCardData: FeaturedCardProps[] = [
     },
     {
       imageUrl:
-        "https://s3-alpha-sig.figma.com/img/0a53/44cd/545819d9ecc9010e758c50420e8bc497?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a~gf5wLT6sF3qbK~1M471BBKIBaovXqT9zwm~9LG6Bwks443db26FnoTmaP757HSJd1pbnV4kczQm3-7St2CoWiRCASpmbF~3xTZAv5LC1ijvPX5RUBaRi-LtIjyfAOrlPoy4AcXqmAFK2vgOHgIryaZcEdngtKQzvgE07ZUzYkkbCOmsPeFBrCcsMPHVXKr9n0b8RUN~5-1YpY-AUsScUJuR4B03XgWbfPA~CdDDHm29z3A13Uw6GfGLh-y52d~76G4airrWed-B7OlWYAvJv~BbG5AJuN2NnRHjCKqKZLODvMl~tccswcuJT5iB-5ZAlEp-BRy8elriUJCNwrrVQ__",
+        "https://storage.googleapis.com/capcons-analytics/Capcons-dev-images/FeaturesCard6.webp",
       title: "Send a Hello!",
       reverse: true,
       index: "06",
