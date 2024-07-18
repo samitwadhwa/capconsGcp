50ff08362a946157500c4ad0c84711676a12f0d9
samitwadhwa <samitwadhwa@gmail.com>
2024-07-18 15:35:21 +0530

    kubernetes commits

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,38 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     domains: ["https://storage.googleapis.com", "https://s3-alpha-sig.figma.com","https://images.unsplash.com"],
+//   },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ["storage.googleapis.com", "s3-alpha-sig.figma.com","images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "https://s3-alpha-sig.figma.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "https://images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      }
+    ],
   },
+  output: "standalone",
 };
 
 export default nextConfig;
